d172f09828e21fb8f05f19154984bb8d4b0240ae
ayaatef11 <ayabadrin667@gmail.com>
2025-02-14 13:31:01 +0200

    add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { FooterComponent } from './pages/footer/footer.component';
 
-const routes: Routes = [
+export const routes: Routes = [//if you don't use expoert it is done locally
   { path: '', component: HomeComponent },  // Default route
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'footer', component: FooterComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' }  // Redirect unknown routes to home
 ];
 

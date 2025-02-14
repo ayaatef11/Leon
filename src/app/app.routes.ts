5d4267bfbf936f30e6a717acb1d25430021927f3
ayaatef11 <ayabadrin667@gmail.com>
2025-02-14 16:20:30 +0200

    fix routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,32 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router'; // Import RouterModule
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { ServicesComponent } from './pages/services/services.component';
-import { PortfolioComponent } from './pages/portfolio/portfolio.component';
 import { FooterComponent } from './pages/footer/footer.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
 
-export const routes: Routes = [//if you don't use expoert it is done locally
-  { path: '', component: HomeComponent },  // Default route
+
+// Define Routes
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'services', component: ServicesComponent },
   { path: 'portfolio', component: PortfolioComponent },
-  { path: 'footer', component: FooterComponent },
-  { path: '**', redirectTo: '', pathMatch: 'full' }  // Redirect unknown routes to home
+  { path: 'footer', component: FooterComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes) // 👈 Important: Register routes
+
+  ],
+  exports: [RouterModule],
+  providers: [],
+  bootstrap: []
 })
-export class AppRoutingModule { }
+export class AppModule { }
